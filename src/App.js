11352a4efc69a3693efae9e23488fece21ae3deb
Prateek yadav <shubhamy9451@gmail.com>
2023-07-18 20:13:30 +0530

    12

--- src/App.js
+++ src/App.js
@@ -41,23 +41,18 @@ import ProductReviews from "./component/Admin/ProductReviews.js";
 import Contact from "./component/layout/Contact/Contact.js";
 import About from "./component/layout/About/About.js";
 import NotFound from "./component/layout/Not Found/NotFound.js";
-import {BackendServer} from "./index"
 function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
-  // const [stripeApiKey, setStripeApiKey] = useState("");
-  const stripePubKey ="pk_test_51NTP1FSDsVZPKzmOcOSiWeikixKfqBHLrTVpne9Y0K1wwNtjuGr2We7W2cnnvJCZwoZGbST3pW9yPpml2XCWmqYc00D7BlyJrz"
-  const stripeApiKey = loadStripe(stripePubKey);
-  //  function getStripeApiKey() {
-  //   async function getStripeApiKey() {
-  //     const { data } = await axios.get(`${BackendServer}/api/v1/stripeapikey`);
-  //     setStripeApiKey(data.stripeApiKey);
-  //     console.log(data);
-  //   }
-  // }
+  const [stripeApiKey, setStripeApiKey] = useState("");
+
+  async function getStripeApiKey() {
+    const { data } = await axios.get("/api/v1/stripeapikey");
+    setStripeApiKey(data.stripeApiKey);
+    console.log(data.stripeApiKey);
+  }
   useEffect(() => {
     store.dispatch(loadUser());
-    // setStripeApiKey("pk_test_51NTP1FSDsVZPKzmOcOSiWeikixKfqBHLrTVpne9Y0K1wwNtjuGr2We7W2cnnvJCZwoZGbST3pW9yPpml2XCWmqYc00D7BlyJrz");
-    // getStripeApiKey();
+    getStripeApiKey();
   }, []);
   return (
     <>
@@ -65,8 +60,8 @@ function App() {
         <Header />
         {isAuthenticated && <UserOptions user={user} />}
 
-        {stripePubKey && (
-          <Elements stripe={stripeApiKey}>
+        {stripeApiKey && (
+          <Elements stripe={loadStripe(stripeApiKey)}>
             <ProtectedRoute exact path="/process/payment" component={Payment} />
           </Elements>
         )}
@@ -112,11 +107,11 @@ function App() {
           <ProtectedRoute isAdmin={true} exact path="/admin/user/:id" component={UpdateUser} />
           <ProtectedRoute isAdmin={true} exact path="/admin/reviews" component={ProductReviews} />
           
-          {/* <Route
+          <Route
           component={
             window.location.pathname === "/process/payment" ? null : NotFound
           }
-        /> */}
+        />
 
         </Switch>
         <Footer />
@@ -126,3 +121,6 @@ function App() {
 }
 
 export default App;
+
+
+
